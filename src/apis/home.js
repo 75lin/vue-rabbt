61f7fcf874de6b6817bcb0e5125ea48603018075
75lin <3145976766@qq.com>
2026-06-13 20:42:11 +0800

    实现轮播图、新鲜好物、热门推荐数据展示

---
+++ src/apis/home.js
@@ -0,0 +1,17 @@
+import httpInstance from '@/utils/http'
+
+export function getBannerAPI(){
+   return httpInstance.get('/home/banner');
+}
+
+export function getHomeNewAPI(){
+   return httpInstance.get('/home/new',{
+      params:{
+         limit: 4,
+      }
+   });
+}
+
+export function getHomeHotAPI(){
+   return httpInstance.get('/home/hot');
+}
---
+++ src/views/Home/components/HomePanel.vue
@@ -0,0 +1,66 @@
+<script setup>
+defineProps({
+    title: {
+        type: String,
+        default: '主标题',
+    },
+    subTitle: {
+        type: String,
+        default: '副标题',
+    }
+})
+</script>
+
+<template>
+    <div class="home-panel">
+        <div class="container">
+            <div class="head">
+                <h3>{{ title }}<small>{{ subTitle }}</small></h3>
+            </div>
+            
+            <div class="body">
+
+
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.home-panel{
+    background: #fff;
+
+    .head{
+        padding: 40px 0;
+        display: flex;
+        align-items: flex-start;
+
+        h3{
+            flex:1;
+            font-size:32px;
+            font-weight: normal;
+            height: 35px;
+            line-height: 35px;
+        }
+        
+        small {
+        font-size: 16px;
+        color: #999;
+        margin-left: 20px;
+      }
+    }
+
+    .body{
+        display: flex;
+        .content{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex:1;
+            height: 200px;
+            background: #eee;
+        }
+    }
+}
+</style>
