188723c1cabdb865de8d936ff0f72fd775c1843d
75lin <3145976766@qq.com>
2026-06-13 21:54:43 +0800

    实现图片懒加载

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import { lazyPlugin } from './directives'
 
 const app = createApp(App)
 
@@ -13,6 +14,6 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
 
 app.use(createPinia())
 app.use(router)
-
+app.use(lazyPlugin);
 
 app.mount('#app')
